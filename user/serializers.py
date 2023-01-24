cc4dd8dcd7dbaeb5c90523f5d375610e705b158e
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-01-24 16:29:48 +0300

    serializer and view added

--- user/serializers.py
+++ user/serializers.py
@@ -8,5 +8,32 @@ class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True,
     validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True) 
+    password2 = serializers.CharField(write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
     class Meta:
         model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password2"
+        )
+
+
+    def validate(self, data):
+        if data["password"] != ["password2"]:
+            raise serializers.ValidationError(
+                {"password":"Password fields didnt match"}
+            )
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        password = validated_data.pop("password")
+        user = User.objects.create(**validated_data) #username=validated_data["username"]
+        user.set_password(password) encrypte yapar
+        user.save()
+        return user
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
